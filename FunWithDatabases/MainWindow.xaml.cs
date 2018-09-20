d0a255a1d02efa5f9e79f125bc4bc1c636330eaf
LUKAS\lukas <lukas.saul.23@gmail.com>
2018-09-20 16:22:14 -0500

    Assets and Employees buttons both print out all of the information for the tables, they would most likely need to be formatted. Need to look at adding ability to insert an employee and asset

--- FunWithDatabases/MainWindow.xaml.cs
+++ FunWithDatabases/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,46 @@ namespace FunWithDatabases
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        OleDbConnection cn;
+        public MainWindow() 
         {
             InitializeComponent();
+            cn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\lukas\Documents\Business1.accdb");
+        }
+
+        private void assetSearch_Click(object sender, RoutedEventArgs e)
+        {
+            string query = "SELECT * FROM Assets";
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+            cn.Open();
+            OleDbDataReader read = cmd.ExecuteReader();
+            string data = "";
+            while (read.Read())
+            {
+                data += read[0].ToString() + " " + read[1].ToString() + " " + read[2].ToString() + " " + read[3].ToString() + "\n";
+            }
+
+            displayStuff.Text = data;
+
+            cn.Close();
+
+        }
+
+        private void employSearch_Click(object sender, RoutedEventArgs e)
+        {
+            string query = "SELECT * FROM Employees";
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+            cn.Open();
+            OleDbDataReader read = cmd.ExecuteReader();
+            string data = "";
+            while (read.Read())
+            {
+                data += read[0].ToString() + " " + read[1].ToString() + " " + read[2].ToString() + "\n";
+            }
+
+            displayStuff.Text = data;
+
+            cn.Close();
         }
     }
 }
